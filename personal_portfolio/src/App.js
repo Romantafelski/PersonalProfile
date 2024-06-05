0cecac1832cc950c6120d767d5f10731947780c7
Roman Tafelski <romantafelski@gmail.com>
2024-06-05 16:24:42 -0700

    adding transparency to background

--- personal_portfolio/src/App.js
+++ personal_portfolio/src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg'
-import MyLogo from './MyLogo.png'
-import MPROSolo from './MPROSolo.jpg'
-import './App.css';
+import logo from "./logo.svg";
+import Background from "./Background.jpg";
+import MyLogo from "./MyLogo.png";
+import MPROSolo from "./MPROSolo.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <menu>
-          <img className='MyLogo' src={MyLogo} alt="Logo" />
-          <h3 className='menuItem'>Home</h3>
-          <h3 className='menuItem'>About Me</h3>
-          <h3 className='menuItem'>Resume</h3>
-          <h3 className='menuItem'>Projects</h3>
-        <img className='me' src={MPROSolo} alt="Roman" />
-        </menu>
-      </header>
+      <img className="background" src={Background} />
+      <div className="circle"></div>
+      <img className="me" src={MPROSolo} alt="Roman" />
+      <menu>
+        <h3 className="menuItem">Home</h3>
+        <h3 className="menuItem">About Me</h3>
+        <h3 className="menuItem">Resume</h3>
+        <h3 className="menuItem">Projects</h3>
+      </menu>
+        <img className="menuItem" id="myLogo" src={MyLogo} alt="Logo" />
     </div>
   );
 }
