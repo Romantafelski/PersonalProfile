27f8592e4d79bcfb97c0b12dab66da73c8c5e294
Roman Tafelski <romantafelski@gmail.com>
2024-06-05 17:33:28 -0700

    finished homepage design

--- personal_portfolio/src/App.js
+++ personal_portfolio/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="circle"></div>
       <img className="me" src={MPROSolo} alt="Roman" />
       <menu>
-        <h3 className="menuItem">Home</h3>
+        <h3 className="menuItem" id="home">Home</h3>
         <h3 className="menuItem">About Me</h3>
         <h3 className="menuItem">Resume</h3>
         <h3 className="menuItem">Projects</h3>
